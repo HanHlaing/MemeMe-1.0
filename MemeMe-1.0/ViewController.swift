5f831ac0f684a4fa229587564209f5d1d9e7f520
Han Hlaing Moe <hanhlaingmoe.hhm27@gmail.com>
2021-07-30 13:54:57 +0630

    Add top and bottom textfields with default text style

--- MemeMe-1.0/ViewController.swift
+++ MemeMe-1.0/ViewController.swift
@@ -7,7 +7,7 @@
 import Foundation
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
 
     // MARK: Outlets
     
@@ -17,9 +17,28 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     @IBOutlet weak var btnCamera: UIBarButtonItem!
     
+    @IBOutlet weak var textFieldTop: UITextField!
+    
+    @IBOutlet weak var textFieldBottom: UITextField!
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -2.0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textFieldTop.delegate = self
+        textFieldBottom.delegate = self
+        textFieldTop.text = "TOP"
+        textFieldBottom.text = "BOTTOM"
+        textFieldTop.textAlignment = .center
+        textFieldBottom.textAlignment = .center
+        textFieldTop.defaultTextAttributes = memeTextAttributes
+        textFieldBottom.defaultTextAttributes = memeTextAttributes
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -46,6 +65,8 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             present(pickerController, animated: true, completion: nil)
         }
     
+    // MARK: Delegate methods
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                     imagePickerView.image = image
@@ -55,6 +76,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
     }
 }
 
